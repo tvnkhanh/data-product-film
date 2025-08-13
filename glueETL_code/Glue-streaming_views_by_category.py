7575b4bf0e2bc9377c9830c485aeded7eaeb4e19
tvnkhanh <tuvankhanh2002@gmail.com>
2025-08-13 18:42:59 +0000

    Test automating deployment 4

--- glueETL_code/Glue-streaming_views_by_category.py
+++ glueETL_code/Glue-streaming_views_by_category.py
@@ -55,6 +55,7 @@ s3output = glueContext.getSink(
 # Test - Modifed!!!
 # Test - Modifed 2!!!
 # Test - Modifed 3!!!
+# Test - Modifed 4!!!
 # Set the database and table name for where you want this table
 # to be registered in the Glue catalog
 s3output.setCatalogInfo(
